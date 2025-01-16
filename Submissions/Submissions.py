411f085485f0c249bdf2fab4dfd736c806c3710e
Jakub Szamotulski <69983735+jszama@users.noreply.github.com>
2025-01-16 11:34:46 +0000

    Delete Submissions directory

--- Submissions/Submissions.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        needed = defaultdict(int)
-
-        for i in range(len(nums)):
-            if nums[i] in needed.keys():
-                return [i, needed[nums[i]]]
-            needed[target-nums[i]] = i
